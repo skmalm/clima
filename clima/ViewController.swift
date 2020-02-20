a504497555d302529280db1762c8ebaefd61ce16
Sebastian Malm <skmalm@gmail.com>
2020-02-20 17:14:37 -0500

    feat: Fix placeholder condition symbol and vary UI appearance for dark/light mode

--- clima/ViewController.swift
+++ clima/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
 }
 
